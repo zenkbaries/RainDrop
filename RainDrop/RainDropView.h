049f2313089a4335daa05d4a642d21e55948e41a
John Towery <kbaries@gmail.com>
2015-12-28 00:20:19 -0700

    Initial Commit

---
+++ RainDrop/RainDropView.h
@@ -0,0 +1,13 @@
+//
+//  RainDropView.h
+//  RainDrop
+//
+//  Created by John Towery on 12/28/15.
+//  Copyright © 2015 John Towery. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface RainDropView : ScreenSaverView
+
+@end
---
+++ RainDrop/RainDropView.m
@@ -0,0 +1,52 @@
+//
+//  RainDropView.m
+//  RainDrop
+//
+//  Created by John Towery on 12/28/15.
+//  Copyright © 2015 John Towery. All rights reserved.
+//
+
+#import "RainDropView.h"
+
+@implementation RainDropView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
